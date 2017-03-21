f6fbece93467b36479b5abe3b611f7dced8b12a6
ryan <emmons.ryan@gmail.com>
2017-03-20 21:45:49 -0400

    Delete kitchenpi-base.js

--- src/kitchenpi-base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const kitchenPiBase = Rebase.createClass({
-    apiKey: "AIzaSyAXnvMx6zJBADLvUXVMpKeTeU6x_WcV7nk",
-    authDomain: "kitchen-pi.firebaseapp.com",
-    databaseURL: "https://kitchen-pi.firebaseio.com"
-});
-
-export default kitchenPiBase;
